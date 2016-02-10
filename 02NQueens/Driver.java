5e2340bf7702a87449b50e4198ca9f924ec1fd7f
Brian Yang <brian.yang328@gmail.com>
2016-02-10 13:03:43 -0500

    Fix NQueens recursion

--- 02NQueens/Driver.java
+++ 02NQueens/Driver.java
@@ -1,8 +1,7 @@
 public class Driver {
     public static void main(String[] args) {
-	Board board = new Board(3);
+	QueenBoard board = new QueenBoard(3);
+	board.solve();
 	System.out.println(board);
-	board.addQueen(0, 0);
-	board.addQueen(1, 1);
     }
 }
---
+++ 02NQueens/QueenBoard.java
@@ -0,0 +1,115 @@
+public class QueenBoard implements hw02 {
+    private int[][] board;
+    private int numQueens;
+
+    public QueenBoard(int size) {
+	board = new int[size][size];
+	numQueens = 0;
+    }
+
+    public String name() {
+	return "Yang,Brian";
+    }
+
+    /**
+     *precondition: board is filled with 0's only.
+     *postcondition: 
+     *-return false, and board is still filled
+     *with 0's for a board that cannot be solved.
+     *-return true, and board is filled with the 
+     *final configuration of the board after adding 
+     *all n queens.
+     */
+    public boolean solve()
+    {
+	return solveH(0);
+    }
+
+    /**
+     *Helper method fr solve. 
+     */
+    private boolean solveH(int col){
+	for (int i = 0; i < board.length; i++) {
+	    if (addQueen(i, col)) {
+		if (col == board.length - 1) {
+		    return true;
+		}
+		if (solveH(col + 1)) {
+		    return true;
+		} else {
+		    removeQueen(i, col);
+		}
+	    }
+	}
+	return false;
+		
+    }
+
+    public void printSolution(){
+	/**Print the board like toString, except
+	      all negative numbers, and 0's are replaced with '_'
+	         and all 1's are replaced with 'Q'
+	*/
+    }
+
+    /********Do Not Edit Below This Line**********************************/
+
+    private boolean addQueen(int row, int col){
+	if(board[row][col] != 0){
+	    return false;
+	}
+	board[row][col] = 1;
+	int offset = 1;
+	while(col+offset < board[row].length){
+	    board[row][col+offset]--;
+	    if(row - offset >= 0){
+		board[row-offset][col+offset]--;
+	    }
+	    if(row + offset < board.length){
+		board[row+offset][col+offset]--;
+	    }
+	    offset++;
+	}
+	numQueens++;
+	return true;
+    }
+
+    private boolean removeQueen(int row, int col){
+	if(board[row][col] != 1){
+	    return false;
+	}
+	board[row][col] = 0;
+	int offset = 1;
+	while(col+offset < board[row].length){
+	    board[row][col+offset]++;
+	    if(row - offset >= 0){
+		board[row-offset][col+offset]++;
+	    }
+	    if(row + offset < board.length){
+		board[row+offset][col+offset]++;
+	    }
+	    offset++;
+	}
+	numQueens--;
+	return true;
+    }
+
+    public String toString(){
+	String ans = "";
+	for(int r = 0; r < board.length; r++){
+	    for(int c = 0; c < board[0].length; c++){
+		ans+= board[r][c]+"\t";
+	    }
+	    ans+="\n";
+	}
+	return ans;
+    }
+
+    /*
+    public static void main(String[]args){
+        Board b = new Board(5);
+	b.solve();
+        System.out.println(b);
+    }
+    */
+}
