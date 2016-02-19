f9ae490ebd4f5b546a33798e15c6e5384e4a4868
Brian Yang <brian.yang328@gmail.com>
2016-02-19 11:08:32 -0500

    Print with leading space for numbers less than 10

--- 03KnightsTour/KnightBoard.java
+++ 03KnightsTour/KnightBoard.java
@@ -61,7 +61,11 @@ public class KnightBoard implements hw03 {
     public void printSolution() {
 	for (int i = 0; i < board.length; i++) {
 	    for (int j = 0; j < board[i].length; j++) {
-		System.out.print(board[i][j] + " ");
+		if (board[i][j] < 10) {
+		    System.out.print("_" + board[i][j] + " ");
+		} else {
+		    System.out.print(board[i][j] + " ");
+		}
 	    }
 	    System.out.println();
 	}
