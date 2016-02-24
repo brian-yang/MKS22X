1bbce045523d5de0845e1eb5b7013d1f31d4b110
Brian Yang <brian.yang328@gmail.com>
2016-02-23 22:32:22 -0500

    Try to read from input file

---
+++ 04USACO/Bronze.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bronze {
+    public static void main(String[] args) {
+	Scanner in = new Scanner("makelake.in");
+	
+	while (in.hasNextLine()) {
+	    Scanner line = new Scanner(in.nextLine());
+	}
+    }
+}
+	
