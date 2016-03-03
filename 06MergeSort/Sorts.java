249d466349ffa47bde1c6f5fa2b15f6ba7291007
Brian Yang <brian.yang328@gmail.com>
2016-03-03 17:13:02 -0500

    Work on merge sort

--- 06MergeSort/Sorts.java
+++ 06MergeSort/Sorts.java
@@ -1,183 +1,61 @@
 public class Sorts {
-    private static boolean debug = false;
-
-    public static void printArray(int[] data) {
-	String arr = "[";
-	for (int i = 0; i < data.length - 1; i++) {
-	    arr += Integer.toString(data[i]) + ", ";
-	}
-	arr += Integer.toString(data[data.length - 1]);
-	arr += "]";
-	System.out.println(arr);
+    public static void main(String[] args) {}
+    public static void mergeSort(int[] data) {
+	mergeSortHelper(data, 0, data.length);
     }
-
-    // Insertion Sort
-    public static void insertionSort(int[] data) {
-	for (int i = 1; i < data.length; i++) {
-	    int tmp = data[i];
-	    int slot = i - 1;
-	    while (slot >= 0 && tmp < data[slot]) {
-		data[slot + 1] = data[slot];
-		slot--;
-	    }
-	    data[slot + 1] = tmp;
-	    // Debug
-	    if (debug) {
-		printArray(data);
-	    }
+    public static void mergeSortHelper(int[] arr, int start, int end) {
+	if (end - start == 1) {
+	    return;
 	}
+	int startA = 0;
+	int endA = arr.length / 2;
+	int startB = arr.length / 2 + 1;
+	int endB = arr.length - 1;
+	mergeSort(arr, startA, endA);
+	mergeSort(arr, startB, endB);
+	Sorts.merge(arr, startA, endA, startB, endB);
     }
+    private static void merge(int[] arr, int startA, int endA, int startB, int endB) {
+	// used to rewrite arr at the end of this function
+	// because startA and startB will have different values
+	int tmpStartA = startA;
+	int tmpStartB = startB;
 
-    // Selection Sort
-    public static void selectionSort(int[] data) {
-	int min, ind, tmp;
-	for (int i = 1; i < data.length; i++) {
-	    String combo = findMin(data, i).replace("\n", "");
-	    min = Integer.parseInt(combo.split(":")[0]);
-	    if (data[i - 1] > min) {
-		// swap first index
-		tmp = data[i - 1];
-		data[i - 1] = min;
-		// swap other element
-		ind = Integer.parseInt(combo.split(":")[1]);
-		data[ind] = tmp;
+	// Merge the two parts of arr
+	int[] newArr = new int[endB - startA + 1];
+	int index = 0;
+	while (startA != endA || startB != endB) {
+	    if (arr[startA] <= arr[startB]) {
+		newArr[index] = arr[startA];
+		startA++;
+	    } else {
+		newArr[index] = arr[startB];
+		startB++;
 	    }
-	    // Debug
-	    if (debug) {
-		printArray(data);
+	    index++;
+	}
+	if (startA == endA) {
+	    for (int i = startB; i < endB; i++) {
+		newArr[index] = arr[startB];
+		index++;
+	    }
+	} else {
+	    for (int i = startA; i < endA; i++) {
+		newArr[index] = arr[startA];
+		index++;
 	    }
 	}
-    }
 
-    private static String findMin(int[] data, int index) {
-	int min = data[index];
-	int minIndex = index;
-	for (int i = index; i < data.length; i++) {
-	    if (min > data[i]) {
-		min = data[i];
-		minIndex = i;
-	    }
+	// Rewrite arr
+	index = 0;
+	for (int i = tmpStartA; i < endA; i++) {
+	    arr[i] = newArr[index];
+	    index++;
 	}
-	return Integer.toString(min) + ":" + Integer.toString(minIndex);
-    }
-
-    // Bubble Sort
-    public static void bubbleSort(int[] data) {
-	int tmp;
-        for (int counter = data.length; counter >= 0; counter--) {
-	    for (int i = 1; i < counter; i++) {
-		if (data[i - 1] > data[i]) {
-		    swap(data, i, i - 1);
-		}
-	    }
-	    // Debug
-	    if (debug) {
-		printArray(data);
-	    }
+	for (int i = tmpStartB; i < endB; i++) {
+	    arr[i] = newArr[index];
+	    index++;
 	}
-    }
 
-    public static void swap(int[] data, int index, int index2) {
-	int tmp = data[index];
-	data[index] = data[index2];
-	data[index2] = tmp;
-    }
-
-    public static void fillRandom(int[] data) {
-	for (int i = 0; i < data.length; i++) {
-	    data[i] = (int) (Math.random() * 10000);
-	}
-    }
-}
-Status API Training Shop Blog About Pricing
-    public class Sorts {
-    private static boolean debug = false;
-
-    public static void printArray(int[] data) {
-	String arr = "[";
-	for (int i = 0; i < data.length - 1; i++) {
-	    arr += Integer.toString(data[i]) + ", ";
-	}
-	arr += Integer.toString(data[data.length - 1]);
-	arr += "]";
-	System.out.println(arr);
-    }
-
-    // Insertion Sort
-    public static void insertionSort(int[] data) {
-	for (int i = 1; i < data.length; i++) {
-	    int tmp = data[i];
-	    int slot = i - 1;
-	    while (slot >= 0 && tmp < data[slot]) {
-		data[slot + 1] = data[slot];
-		slot--;
-	    }
-	    data[slot + 1] = tmp;
-	    // Debug
-	    if (debug) {
-		printArray(data);
-	    }
-	}
-    }
-
-    // Selection Sort
-    public static void selectionSort(int[] data) {
-	int min, ind, tmp;
-	for (int i = 1; i < data.length; i++) {
-	    String combo = findMin(data, i).replace("\n", "");
-	    min = Integer.parseInt(combo.split(":")[0]);
-	    if (data[i - 1] > min) {
-		// swap first index
-		tmp = data[i - 1];
-		data[i - 1] = min;
-		// swap other element
-		ind = Integer.parseInt(combo.split(":")[1]);
-		data[ind] = tmp;
-	    }
-	    // Debug
-	    if (debug) {
-		printArray(data);
-	    }
-	}
-    }
-
-    private static String findMin(int[] data, int index) {
-	int min = data[index];
-	int minIndex = index;
-	for (int i = index; i < data.length; i++) {
-	    if (min > data[i]) {
-		min = data[i];
-		minIndex = i;
-	    }
-	}
-	return Integer.toString(min) + ":" + Integer.toString(minIndex);
-    }
-
-    // Bubble Sort
-    public static void bubbleSort(int[] data) {
-	int tmp;
-        for (int counter = data.length; counter >= 0; counter--) {
-	    for (int i = 1; i < counter; i++) {
-		if (data[i - 1] > data[i]) {
-		    swap(data, i, i - 1);
-		}
-	    }
-	    // Debug
-	    if (debug) {
-		printArray(data);
-	    }
-	}
-    }
-
-    public static void swap(int[] data, int index, int index2) {
-	int tmp = data[index];
-	data[index] = data[index2];
-	data[index2] = tmp;
-    }
-
-    public static void fillRandom(int[] data) {
-	for (int i = 0; i < data.length; i++) {
-	    data[i] = (int) (Math.random() * 10000);
-	}
     }
 }
