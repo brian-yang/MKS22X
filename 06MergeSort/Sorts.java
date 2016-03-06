98cf3945d9e93d9a9b76c596ca25a27f6e4b6409
Brian Yang <brian.yang328@gmail.com>
2016-03-05 22:00:26 -0500

    Implement merge sort

--- 06MergeSort/Sorts.java
+++ 06MergeSort/Sorts.java
@@ -1,20 +1,29 @@
+import java.util.*;
+
 public class Sorts {
-    public static void main(String[] args) {}
+    // public static void main(String[] args) {
+    // 	int[] test = {262,287, -13461357, -12, 15754561,27,3146,61,17};
+    // 	//Sorts.merge(test, 0, 2, 3, 5);
+    // 	Sorts.mergeSort(test);
+    // 	System.out.println(Arrays.toString(test));
+    // }
+
     public static void mergeSort(int[] data) {
-	mergeSortHelper(data, 0, data.length);
+	mergeSortHelper(data, 0, data.length - 1);
     }
-    public static void mergeSortHelper(int[] arr, int start, int end) {
-	if (end - start == 1) {
-	    return;
+
+    private static void mergeSortHelper(int[] arr, int start, int end) {
+	if (end > start) {
+	    int startA = start;
+	    int endA = (end + start) / 2;
+	    int startB = (end + start) / 2 + 1;
+	    int endB = end;
+	    mergeSortHelper(arr, startA, endA);
+	    mergeSortHelper(arr, startB, endB);
+	    merge(arr, startA, endA, startB, endB);
 	}
-	int startA = 0;
-	int endA = arr.length / 2;
-	int startB = arr.length / 2 + 1;
-	int endB = arr.length - 1;
-	mergeSort(arr, startA, endA);
-	mergeSort(arr, startB, endB);
-	Sorts.merge(arr, startA, endA, startB, endB);
     }
+
     private static void merge(int[] arr, int startA, int endA, int startB, int endB) {
 	// used to rewrite arr at the end of this function
 	// because startA and startB will have different values
@@ -24,7 +33,7 @@ public class Sorts {
 	// Merge the two parts of arr
 	int[] newArr = new int[endB - startA + 1];
 	int index = 0;
-	while (startA != endA || startB != endB) {
+	while (startA <= endA && startB <= endB) {
 	    if (arr[startA] <= arr[startB]) {
 		newArr[index] = arr[startA];
 		startA++;
@@ -34,25 +43,27 @@ public class Sorts {
 	    }
 	    index++;
 	}
-	if (startA == endA) {
-	    for (int i = startB; i < endB; i++) {
-		newArr[index] = arr[startB];
+	if (startA > endA) {
+	    for (int i = startB; i <= endB; i++) {
+		newArr[index] = arr[i];
 		index++;
 	    }
-	} else {
-	    for (int i = startA; i < endA; i++) {
-		newArr[index] = arr[startA];
+	} else if (startB > endB) {
+	    for (int i = startA; i <= endA; i++) {
+		newArr[index] = arr[i];
 		index++;
 	    }
 	}
 
+	//System.out.println(Arrays.toString(newArr));
+
 	// Rewrite arr
 	index = 0;
-	for (int i = tmpStartA; i < endA; i++) {
+	for (int i = tmpStartA; i <= endA; i++) {
 	    arr[i] = newArr[index];
 	    index++;
 	}
-	for (int i = tmpStartB; i < endB; i++) {
+	for (int i = tmpStartB; i <= endB; i++) {
 	    arr[i] = newArr[index];
 	    index++;
 	}
