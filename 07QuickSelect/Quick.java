e7603e4b0bedc4cd449a92eeaddaaddbb1ad01f6
Brian Yang <brian.yang328@gmail.com>
2016-03-11 17:41:54 -0500

    Fix quicksort to deal with repeating elements

--- 07QuickSelect/Quick.java
+++ 07QuickSelect/Quick.java
@@ -1,13 +1,21 @@
 import java.util.*;
 
 public class Quick {
-    // public static void main(String[] args) {
-    // 	int[] data = {0, 10, 100, -15, 20, -2, 1000, 20, 31, -500, 32};
-    // 	quickSort(data);
-    // 	//System.out.println(quickselect(data, Integer.parseInt(args[0])));
-    // 	//System.out.println(partition(data, 0, data.length - 1));
-    // 	System.out.println(Arrays.toString(data));
-    // }
+    public static void main(String[] args) {
+    	int[] data = new int[40000];
+	int[] copy = new int[data.length];
+	for (int i = 0; i < data.length; i++) {
+	    data[i] = (int) (Math.random() * 10000);
+	    copy[i] = data[i];
+	}
+    	//System.out.println(quickselect(data, Integer.parseInt(args[0])));
+    	//System.out.println(Arrays.toString(partition(data, 0, data.length - 1)));
+    	quickSort(data);
+	Arrays.sort(copy);
+
+	System.out.println("Done! Sorted? " + Arrays.equals(data, copy));
+    	//System.out.println(Arrays.toString(data));
+    }
 
     public static String name() {
 	return "7,Yang,Brian";
@@ -18,15 +26,15 @@ public class Quick {
      * ============
      */
     public static void quickSort(int[] data) {
-	quickSort(data, 0, data.length - 1);
+    	quickSort(data, 0, data.length - 1);
     }
 
     private static void quickSort(int[] data, int left, int right) {
-	if (left < right) {
-	    int pivot = partition(data, left, right);
-	    quickSort(data, left, pivot - 1);
-	    quickSort(data, pivot + 1, right);
-	}
+    	if (left < right) {
+    	    int[] pivots = partition(data, left, right);
+    	    quickSort(data, left, pivots[0] - 1);
+    	    quickSort(data, pivots[1] + 1, right);
+    	}
     }
 
     /* ============
@@ -34,65 +42,63 @@ public class Quick {
      * ============
      */
     public static int quickselect(int[] data, int k) {
-	if (k <= 0) {
-	    k = 1;
-	}
-	return quickselect(data, k - 1, 0, data.length - 1);
+    	if (k <= 0) {
+    	    k = 1;
+    	}
+    	return quickselect(data, k - 1, 0, data.length - 1);
     }
 
     private static int quickselect(int[] data, int k, int left, int right) {
-	int partitionIndex = partition(data, left, right);
-	if (partitionIndex == k) {
-	    return data[partitionIndex];
-	} else if (partitionIndex > k) {
-	    return quickselect(data, k, left, partitionIndex - 1);
-	} else if (partitionIndex < k) {
-	    return quickselect(data, k, partitionIndex + 1, right);
+    	int[] partitionIndex = partition(data, left, right);
+    	if (partitionIndex[0] <= k && partitionIndex[1] >= k) {
+    	    return data[partitionIndex[0]];
+    	} else if (partitionIndex[0] > k) {
+	    return quickselect(data, k, left, partitionIndex[0] - 1);
+	} else if (partitionIndex[1] < k) {
+	    return quickselect(data, k, partitionIndex[1] + 1, right);
 	}
-	return 0;
+    	return 0;
     }
 
-    private static int partition(int[] data, int left, int right) {
+    private static int[] partition(int[] data, int left, int right) {
 	//System.out.println("Original Array: " + Arrays.toString(data));
-
-	// Check for final value
-	if (left == right) {
-	    return left;
-	}
-
 	// Random pivot index
 	int pivot = (int) (Math.random() * (right - left + 1)) + left;
 	//System.out.println("Pivot: " + pivot);
-
-	// Move pivot value to corner
-	swap(data, pivot, left);
+	int[] data2 = new int[data.length];
 
 	// Partition
-	int indexL = left + 1;
+	int dataIndex = left;
+	int indexL = left;
 	int indexR = right;
-	while (indexL < indexR) {
+
+	while (dataIndex <= right) {
 	    // System.out.println("Partitioning...");
 	    // System.out.println(Arrays.toString(data));
 	    // System.out.println("====================");
-	    if (data[indexL] <= data[left]) {
+	    if (data[dataIndex] < data[pivot]) {
+		data2[indexL] = data[dataIndex];
 		indexL++;
-	    } else if (data[indexL] > data[left]) {
-		swap(data, indexR, indexL);
+	    } else if (data[dataIndex] > data[pivot]) {
+		data2[indexR] = data[dataIndex];
 		indexR--;
 	    }
+	    dataIndex++;
+	}
+	for (int i = indexL; i <= indexR; i++) {
+	    data2[i] = data[pivot];
 	}
 
-	// if (indexL != indexR) {
-	//     System.out.println("Error!");
-	// }
-
-
-	if (data[indexL] > data[left]) {
-	    swap(data, indexL - 1, left);
-	    return indexL - 1;
+	// Swap back
+	for (int i = left; i <= right; i++) {
+	    data[i] = data2[i];
 	}
-	swap(data, indexL, left);
-	return indexL;
+
+	// Get left index and right index
+	int[] indices = new int[2];
+	indices[0] = indexL;
+	indices[1] = indexR;
+	return indices;
     }
 
     private static void swap(int[] data, int ind1, int ind2) {
---
+++ 07QuickSelect/QuickOld.java
@@ -0,0 +1,114 @@
+import java.util.*;
+
+public class QuickOld {
+    public static void main(String[] args) {
+    	int[] data = new int[100];
+	int[] copy = new int[data.length];
+	for (int i = 0; i < data.length; i++) {
+	    data[i] = (int) (Math.random() * 100);
+	    copy[i] = data[i];
+	}
+    	//System.out.println(quickselectOld(data, Integer.parseInt(args[0])));
+    	//System.out.println(partitionOld(data, 0, data.length - 1));
+    	quickSortOld(data);
+	Arrays.sort(copy);
+
+	System.out.println("Done! Sorted? " + Arrays.equals(data, copy));
+    	//System.out.println(Arrays.toString(data));
+    }
+
+    public static String name() {
+	return "7,Yang,Brian";
+    }
+
+
+    // OLD VERSION OF QUICK SORT
+
+    /* ============
+     * QUICK SORT
+     * ============
+     */
+    public static void quickSortOld(int[] data) {
+	quickSortOld(data, 0, data.length - 1);
+    }
+
+    private static void quickSortOld(int[] data, int left, int right) {
+	if (left < right) {
+	    int pivot = partitionOld(data, left, right);
+	    quickSortOld(data, left, pivot - 1);
+	    quickSortOld(data, pivot + 1, right);
+	}
+    }
+
+    /* ============
+     * QUICK SELECT
+     * ============
+     */
+    public static int quickselectOld(int[] data, int k) {
+	if (k <= 0) {
+	    k = 1;
+	}
+	return quickselectOld(data, k - 1, 0, data.length - 1);
+    }
+
+    private static int quickselectOld(int[] data, int k, int left, int right) {
+	int partitionIndex = partitionOld(data, left, right);
+	if (partitionIndex == k) {
+	    return data[partitionIndex];
+	} else if (partitionIndex > k) {
+	    return quickselectOld(data, k, left, partitionIndex - 1);
+	} else if (partitionIndex < k) {
+	    return quickselectOld(data, k, partitionIndex + 1, right);
+	}
+	return 0;
+    }
+
+    private static int partitionOld(int[] data, int left, int right) {
+	//System.out.println("Original Array: " + Arrays.toString(data));
+
+	// Check for final value
+	if (left == right) {
+	    return left;
+	}
+
+	// Random pivot index
+	int pivot = (int) (Math.random() * (right - left + 1)) + left;
+	//System.out.println("Pivot: " + pivot);
+
+	// Move pivot value to corner
+	swap(data, pivot, left);
+
+	// Partition
+	int indexL = left + 1;
+	int indexR = right;
+	while (indexL < indexR) {
+	    // System.out.println("Partitioning...");
+	    // System.out.println(Arrays.toString(data));
+	    // System.out.println("====================");
+	    if (data[indexL] <= data[left]) {
+		indexL++;
+	    } else if (data[indexL] > data[left]) {
+		swap(data, indexR, indexL);
+		indexR--;
+	    }
+	}
+
+	// if (indexL != indexR) {
+	//     System.out.println("Error!");
+	// }
+
+
+	if (data[indexL] > data[left]) {
+	    swap(data, indexL - 1, left);
+	    return indexL - 1;
+	}
+	swap(data, indexL, left);
+	return indexL;
+    }
+
+    private static void swap(int[] data, int ind1, int ind2) {
+	int tmp = data[ind1];
+	data[ind1] = data[ind2];
+	data[ind2] = tmp;
+    }
+}
