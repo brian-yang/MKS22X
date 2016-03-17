b9a756a3a56bcf657052de2f55128da24160b53d
Brian Yang <brian.yang328@gmail.com>
2016-03-16 22:59:17 -0400

    Add basic methods and test methods

--- 08LinkedList/MyLinkedList.java
+++ 08LinkedList/MyLinkedList.java
@@ -1,25 +1,93 @@
 public class MyLinkedList {
     private LNode head;
- 
+    private int size;
+
+    public static void main(String[] args) {
+	MyLinkedList l = new MyLinkedList();
+	for (int i = 0; i < 10; i++) {
+	    l.add((int) (Math.random() * 100));
+	}
+	// toString test
+	System.out.println(l);
+	// size(), get(int index) tests
+	for (int i = 0; i < l.size(); i++) {
+	    System.out.println(l.get(i));
+	}
+	System.out.println();
+	// set(int index, int newValue) test
+	for (int i = 0; i < l.size(); i++) {
+	    System.out.println(l.set(i, 100));
+	}
+	System.out.println(l);
+    }
+
+    // BASIC GET AND SET METHODS
+
+    public int size() {
+	return size;
+    }
+
+    public int get(int index) {
+	LNode cur = head;
+	for (int i = 0; i < index; i++) {
+	    cur = cur.getNext();
+	}
+	return cur.getValue();
+    }
+
+    public int set(int index, int newValue) {
+	LNode cur = head;
+	for (int i = 0; i < index; i++) {
+	    cur = cur.getNext();
+	}
+	int old = cur.getValue();
+	cur.setValue(newValue);
+	return old;
+    }
+
+    // ADD TO END AND TOSTRING METHODS
+
     public boolean add(int value) {
 	if (head == null) {
 	    head = new LNode(value);
+	    size = 1;
 	    return true;
-	} else {	    
+	} else {
+	    LNode cur = head;
+	    while (cur.getNext() != null) {
+		cur = cur.getNext();
+	    }
+	    cur.setNext(new LNode(value));
 	}
-	return false;
+	size += 1;
+	return true;
     }
-   
+
     public String toString() {
 	String arr = "[";
+	LNode p = head;
+	while(p != null){
+	    arr += p.getValue();
+	    if (p.getNext() != null){
+		arr += ", ";
+	    }
+	    p = p.getNext();
+	}
 	arr += "]";
 	return arr;
     }
 
+    // ================================================
+    // ================================================
+
+    /*
+     * LNODE INNER CLASS
+     */
+
     private class LNode {
 	private int num;
 	private LNode node;
-	
+
 	public LNode(int num) {
 	    this.num = num;
 	}
