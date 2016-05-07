b1a39477dae7dc84c7eec79e662e142eddb2ce9d
Brian Yang <brian.yang328@gmail.com>
2016-05-06 23:46:04 -0400

    Implement basic heap functions

---
+++ 13MyHeap/Driver.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args) {
+	Integer[] arr = new Integer[10];
+	for (int i = 0; i < 10; i++) {
+	    arr[i] = (int) (Math.random() * 100);
+	}
+	System.out.println(Arrays.toString(arr));
+	MyHeap<Integer> h = new MyHeap<Integer>(arr);
+	System.out.println(h.size());
+	System.out.println(h);
+	h.add(15);
+	h.add(20);
+	h.add(100);
+	System.out.println(h);
+	h.delete();
+	h.add(300);
+	System.out.println(h);
+	System.out.println(h.size());
+	h.delete();
+	System.out.println(h);
+    }
+}
--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -11,18 +11,26 @@ public class MyHeap<T extends Comparable<T>> {
     }
 
     public MyHeap(T[] array) {
-	size = array.length + 1;
-	data = (T[]) new Comparable[size];
-	for (int i = 1; i < size; i++) {
+	size = array.length;
+	data = (T[]) new Comparable[size + 1];
+	for (int i = 1; i < size + 1; i++) {
 	    data[i] = array[i - 1];
 	}
 	heapify();
     }
 
+    public int size() {
+	return size;
+    }
+
+    public T peek() {
+	return data[1];
+    }
+
     /**pushDown
       precondition: data is a heap with at most one item
       out of place (element at k)
-      postconditions:-the element at index k has been 
+      postconditions:-the element at index k has been
                      shifted to the correct spot.
                      -data is a valid heap
     **/
@@ -30,14 +38,22 @@ public class MyHeap<T extends Comparable<T>> {
 	if (size >= 2*k) {
 	    if (size >= 2*k + 1) {
 		int swapIndex = 0;
-		if (data[2*k] >= data[2*k + 1]) {
+		if (data[2*k].compareTo(data[2*k + 1]) >= 0) {
 		    swapIndex = 2*k;
 		} else {
 		    swapIndex = 2*k + 1;
 		}
-		swap(k, swapIndex);
+		// checks if value should be pushed down
+		if (data[k].compareTo(data[swapIndex]) < 0) {
+		    swap(k, swapIndex);
+		    pushDown(swapIndex); // recursive call
+		}
 	    } else {
-		swap(k, 2*k);
+		// checks if value should be pushed down
+		if (data[k].compareTo(data[2*k]) < 0) {
+		    swap(k, 2*k);
+		    pushDown(2*k); // recursive call
+		}
 	    }
 	}
     }
@@ -45,32 +61,69 @@ public class MyHeap<T extends Comparable<T>> {
     /**pushUp
        precondition: data is a heap with at most one item
        out of place (element at k)
-       postconditions:-the element at index k has been 
+       postconditions:-the element at index k has been
        shifted to the correct spot.
        -data is a valid heap
     **/
     private void pushUp(int k) {
-	swap(k, k/2);
+	if (k >= 2) {
+	    if (data[k].compareTo(data[k/2]) >= 1) {
+		swap(k, k/2);
+		pushUp(k/2); // recursive call
+	    }
+	}
     }
 
     private void swap(int ind1, int ind2) {
-	int tempVal = data[ind1];
+	T tempVal = data[ind1];
 	data[ind1] = data[ind2];
 	data[ind2] = tempVal;
     }
 
     private void heapify() {
-	
+	for (int i = size/2; i > 0; i--) {
+	    pushDown(i);
+	}
     }
 
     // delete the root
-    public T delete() {}
+    public T delete() {
+	if (size == 0) {
+	    throw new NoSuchElementException();
+	}
+	T oldValue = data[1];
+	if (size == 1) {
+	    data[1] = null;
+	    return oldValue;
+	}
+	data[1] = data[size];
+	data[size] = null;
+	size--;
+	pushDown(1);
+	return oldValue;
+    }
 
-    public void add(T x) {}
-    private void doubleSize() {}
-    public String toString() {}
+    public void add(T x) {
+	if (size + 1 > data.length - 1) {
+	    doubleSize();
+	}
+	data[size + 1] = x;
+	pushUp(size + 1);
+	size++;
+    }
 
-    //do this last
+    private void doubleSize() {
+	T[] newData = (T[]) new Comparable[data.length * 2];
+	for (int i = 1; i < size + 1; i++) {
+	    newData[i] = data[i];
+	}
+	data = newData;
+    }
+
+    public String toString() {
+	return Arrays.toString(data);
+    }
+
     public MyHeap(boolean isMax) {}
     public MyHeap(T[] array, boolean isMax) {}
 }
