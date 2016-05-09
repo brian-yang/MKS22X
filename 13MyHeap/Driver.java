2c8f8e25cf46d2d26088a14a1c8b6bc09bb2ed11
Brian Yang <brian.yang328@gmail.com>
2016-05-09 18:41:04 -0400

    Test constructors with no array parameter

--- 13MyHeap/Driver.java
+++ 13MyHeap/Driver.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 public class Driver {
     public static void main(String[] args) {
-	Integer[] arr = new Integer[10];
-	for (int i = 0; i < 10; i++) {
-	    arr[i] = (int) (Math.random() * 100);
-	}
-	System.out.println(Arrays.toString(arr));
-	MyHeap<Integer> h = new MyHeap<Integer>(arr, false);
+	// Integer[] arr = new Integer[10];
+	// for (int i = 0; i < 10; i++) {
+	//     arr[i] = (int) (Math.random() * 100);
+	// }
+	// System.out.println(Arrays.toString(arr));
+	MyHeap<Integer> h = new MyHeap<Integer>();
 	System.out.println(h.size());
 	System.out.println(h);
 	h.add(15);
